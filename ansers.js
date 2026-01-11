b9247c19c2f1e1a8e3fe91858275945343381150
jackandrew82 <ja.germain2024@gmail.com>
2026-01-11 15:57:32 -0500

    Create ansers.js

---
+++ ansers.js
@@ -0,0 +1,20 @@
+// answers.js
+// Clé de correction à compléter pour chaque brevet
+
+const ANSWERS = {
+  A: {
+    // Exemples à partir de A-CORRIGE.pdf
+    "2": "d", // Toutes ces réponses
+    "3": "d", // respirations lentes, contrôlées, profondes
+    "4": "a", // Préparation, sortie, chute libre, ...
+    "5": "c", // conscient de ses capacités et limites
+    "6": "a"  // Décider ce que vous allez faire dans ce saut
+    // Tu complèteras plus tard les autres (7,8,9,...)
+  },
+  B: {
+    // À remplir avec B-CORRIGE.pdf
+  },
+  C: {
+    // À remplir avec C-CORRIGE.pdf
+  }
+};
