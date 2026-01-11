835202b0088bb8a4199d1098c5f44dff3943c76f
jackandrew82 <ja.germain2024@gmail.com>
2026-01-11 17:19:24 -0500

    Update ansers.js

--- ansers.js
+++ ansers.js
@@ -6,15 +6,15 @@ const ANSWERS = {
     // Exemples à partir de A-CORRIGE.pdf
     "2": "d", // Toutes ces réponses
     "3": "d", // respirations lentes, contrôlées, profondes
-    "4": "a", // Préparation, sortie, chute libre, ...
-    "5": "c", // conscient de ses capacités et limites
-    "6": "a"  // Décider ce que vous allez faire dans ce saut
-    // Tu complèteras plus tard les autres (7,8,9,...)
+    "4": "a",
+    "5": "c",
+    "6": "a"
+    // Tu ajouteras: "7": "...", "8": "...", etc.
   },
   B: {
-    // À remplir avec B-CORRIGE.pdf
+    // À remplir plus tard avec B-CORRIGE.pdf
   },
   C: {
-    // À remplir avec C-CORRIGE.pdf
+    // À remplir plus tard avec C-CORRIGE.pdf
   }
 };
