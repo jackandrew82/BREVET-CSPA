c16efd675f5a1f5f7640d700f8eb593f91b59c66
jackandrew82 <ja.germain2024@gmail.com>
2026-01-12 11:30:30 -0500

    Update generate-answer-sheet.js

--- functions/generate-answer-sheet.js
+++ functions/generate-answer-sheet.js
@@ -1,69 +1,97 @@
 const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
-const fs = require('fs').promises;
-const path = require('path');
 
 async function generateAnswerSheetPDF(studentName, brevet, mistakes, allAnswers) {
-  const templatePath = path.join(__dirname, brevet + '-FEUILLE-REPONSE.pdf');
-  const templateBytes = await fs.readFile(templatePath);
-  
-  const pdfDoc = await PDFDocument.load(templateBytes);
-  const pages = pdfDoc.getPages();
-  const firstPage = pages[0];
-  const { width, height } = firstPage.getSize();
+  const pdfDoc = await PDFDocument.create();
+  let page = pdfDoc.addPage([595, 842]); // A4
+  const { width, height } = page.getSize();
   
   const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
   const boldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
   
-  // GRILLE DE CALIBRATION
-  const gridSize = 20; // Espacement grille (pixels)
-  const cols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let y = height - 60;
   
-  // Lignes horizontales avec numeros
-  for (let i = 0; i <= 40; i++) {
-    const y = height - (i * gridSize);
+  // En-tete
+  page.drawRectangle({
+    x: 0,
+    y: height - 100,
+    width: width,
+    height: 100,
+    color: rgb(0.95, 0.95, 0.95),
+  });
+  
+  page.drawText('FEUILLE DE REPONSES', {
+    x: 50,
+    y: y,
+    size: 20,
+    font: boldFont,
+    color: rgb(0.2, 0.2, 0.2),
+  });
+  
+  y -= 30;
+  
+  page.drawText('Etudiant: ' + studentName + '  |  Brevet: ' + brevet, {
+    x: 50,
+    y: y,
+    size: 12,
+    font: boldFont,
+  });
+  
+  y -= 50;
+  
+  // Map des erreurs
+  const errorsMap = {};
+  mistakes.forEach(m => {
+    errorsMap[m.question] = true;
+  });
+  
+  // Afficher toutes les reponses (Q1-Q60)
+  for (let qNum = 1; qNum <= 60; qNum++) {
+    const qKey = 'q' + qNum;
+    const answer = allAnswers[qKey] || '';
     
-    // Ligne grise
-    firstPage.drawLine({
-      start: { x: 0, y: y },
-      end: { x: width, y: y },
-      thickness: 0.3,
-      color: rgb(0.7, 0.7, 0.7),
-    });
+    if (y < 80) {
+      page = pdfDoc.addPage([595, 842]);
+      y = height - 60;
+    }
     
-    // Numero ligne
-    if (i % 2 === 0) {
-      firstPage.drawText('' + i, {
-        x: 5,
-        y: y - 5,
-        size: 7,
-        font,
-        color: rgb(0, 0, 1),
+    const isError = errorsMap[qNum];
+    
+    // Rectangle rouge si erreur
+    if (isError && answer) {
+      page.drawRectangle({
+        x: 45,
+        y: y - 3,
+        width: 505,
+        height: 18,
+        color: rgb(1, 0.9, 0.9),
+        borderColor: rgb(1, 0, 0),
+        borderWidth: 1,
       });
     }
-  }
-  
-  // Colonnes verticales avec lettres
-  for (let i = 0; i < 25; i++) {
-    const x = i * gridSize;
     
-    // Ligne grise
-    firstPage.drawLine({
-      start: { x: x, y: 0 },
-      end: { x: x, y: height },
-      thickness: 0.3,
-      color: rgb(0.7, 0.7, 0.7),
+    // Question numero
+    page.drawText('Question ' + qNum + ':', {
+      x: 50,
+      y: y,
+      size: 11,
+      font: boldFont,
+      color: rgb(0, 0, 0),
     });
     
-    // Lettre colonne
-    if (i < cols.length) {
-      firstPage.drawText(cols[i], {
-        x: x + 5,
-        y: height - 15,
-        size: 7,
-        font,
-        color: rgb(0, 0, 1),
-      });
-    }
+    // Reponse
+    const displayAnswer = answer || '(pas de reponse)';
+    const answerColor = isError ? rgb(1, 0, 0) : rgb(0, 0, 0);
+    const answerFont = isError ? boldFont : font;
+    
+    page.drawText(displayAnswer.toUpperCase(), {
+      x: 150,
+      y: y,
+      size: 11,
+      font: answerFont,
+      color: answerColor,
+    });
+    
+    y -= 20;
   }
   
   const pdfBytes = await pdfDoc.save();
