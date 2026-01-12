815e27d574b58b66b42795cbaba39911093ccfb0
jackandrew82 <ja.germain2024@gmail.com>
2026-01-11 21:49:27 -0500

    Create questions-data.js

---
+++ functions/questions-data.js
@@ -0,0 +1,58 @@
+const QUESTIONS = {
+  A: {
+    "2": {
+      question: "Lesquelles de ces activités de préparation sont appropriées avant un saut ?",
+      choices: {
+        a: "Visualisation seulement",
+        b: "Étirements seulement",
+        c: "Révision du plan de vol seulement",
+        d: "Toutes ces réponses"
+      }
+    },
+    "3": {
+      question: "Quel type de respiration est recommandé avant un saut ?",
+      choices: {
+        a: "Respirations rapides et courtes",
+        b: "Respirations normales",
+        c: "Retenir sa respiration",
+        d: "Respirations lentes, contrôlées et profondes"
+      }
+    },
+    "4": {
+      question: "Quelles sont les 5 étapes principales d'un saut en parachutisme ?",
+      choices: {
+        a: "Préparation, sortie, chute libre, ouverture, atterrissage",
+        b: "Montée, sortie, vol, descente, atterrissage",
+        c: "Équipement, vol, saut, parachute, sol",
+        d: "Briefing, avion, saut, voilure, retour"
+      }
+    },
+    "5": {
+      question: "Un parachutiste responsable doit être :",
+      choices: {
+        a: "Toujours en compétition",
+        b: "Toujours le plus rapide",
+        c: "Conscient de ses capacités et limites",
+        d: "Toujours seul"
+      }
+    },
+    "6": {
+      question: "Avant chaque saut, il est important de :",
+      choices: {
+        a: "Décider ce que vous allez faire dans ce saut",
+        b: "Sauter sans plan",
+        c: "Improviser en l'air",
+        d: "Suivre les autres sans réfléchir"
+      }
+    }
+    // Tu ajouteras les autres questions progressivement
+  },
+  B: {
+    // Questions du brevet B (à remplir plus tard)
+  },
+  C: {
+    // Questions du brevet C (à remplir plus tard)
+  }
+};
+
+module.exports = { QUESTIONS };
