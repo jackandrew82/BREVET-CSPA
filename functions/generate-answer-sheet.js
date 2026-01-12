af27d3a2b7a7e0069d44f7cc9d3ab18cf4a18b4e
jackandrew82 <ja.germain2024@gmail.com>
2026-01-11 23:58:14 -0500

    Update generate-answer-sheet.js

--- functions/generate-answer-sheet.js
+++ functions/generate-answer-sheet.js
@@ -1,9 +1,9 @@
-const { PDFDocument, rgb } = require('pdf-lib');
+const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 const fs = require('fs').promises;
 const path = require('path');
 const { ANSWER_POSITIONS } = require('./answer-positions');
 
-async function generateAnswerSheetPDF(studentName, brevet, mistakes) {
+async function generateAnswerSheetPDF(studentName, brevet, mistakes, allAnswers) {
   const templatePath = path.join(__dirname, brevet + '-FEUILLE-REPONSE.pdf');
   const templateBytes = await fs.readFile(templatePath);
   
@@ -12,34 +12,59 @@ async function generateAnswerSheetPDF(studentName, brevet, mistakes) {
   const firstPage = pages[0];
   const { width, height } = firstPage.getSize();
   
-  // Ajouter nom etudiant en haut a droite
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const boldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+  
+  // Nom etudiant en haut a droite
   firstPage.drawText('Nom: ' + studentName, {
     x: width - 250,
     y: height - 30,
     size: 11,
+    font: boldFont,
     color: rgb(0, 0, 0),
   });
   
-  // Cercles rouges sur numeros questions erronees
   const positions = ANSWER_POSITIONS[brevet] || {};
   
-  mistakes.forEach(mistake => {
-    const qNum = mistake.question;
+  // Marquer TOUTES les reponses (bonnes en vert, mauvaises en rouge)
+  const mistakesMap = {};
+  mistakes.forEach(m => {
+    mistakesMap[m.question] = {
+      user: m.user_answer,
+      correct: m.right_answer
+    };
+  });
+  
+  // Pour chaque question repondue
+  for (const [qNum, userAnswer] of Object.entries(allAnswers)) {
+    if (!userAnswer || userAnswer === '') continue;
+    
+    const isMistake = mistakesMap[qNum];
+    const color = isMistake ? rgb(1, 0, 0) : rgb(0, 0.6, 0); // Rouge si erreur, vert si bon
     
     if (positions[qNum]) {
       const pos = positions[qNum];
       const targetPage = pages[pos.page] || firstPage;
       
-      // Cercle rouge autour numero question
+      // Cercle autour numero question
       targetPage.drawCircle({
         x: pos.x,
         y: pos.y,
         size: 12,
-        borderColor: rgb(1, 0, 0),
-        borderWidth: 2,
+        borderColor: color,
+        borderWidth: 2.5,
+      });
+      
+      // Afficher la reponse cochee a cote
+      targetPage.drawText('[' + userAnswer + ']', {
+        x: pos.x + 20,
+        y: pos.y - 4,
+        size: 10,
+        font: boldFont,
+        color: color,
       });
     }
-  });
+  }
   
   const pdfBytes = await pdfDoc.save();
   return Buffer.from(pdfBytes);
